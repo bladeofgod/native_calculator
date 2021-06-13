a3c7571215ccd05270ae50530981cdc626b6e9a0
bladeofgod <450103187@qq.com>
2021-06-13 16:57:38 +0800

    layout xml

--- app/src/main/java/com/bedrock/nativecalculatordemo/MainActivity.java
+++ app/src/main/java/com/bedrock/nativecalculatordemo/MainActivity.java
@@ -23,9 +23,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
     }
 
     /**
